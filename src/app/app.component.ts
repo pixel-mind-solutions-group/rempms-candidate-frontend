a5283b2ce251e578552994105e49d897b7230b3b
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-07-05 05:12:15 +0530

    change right s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Title } from '@angular/platform-browser';
   template: '<router-outlet></router-outlet>',
 })
 export class AppComponent implements OnInit {
-  title = 'PIXEL HIRE WEB';
+  title = 'PixelMind Candidate Portal';
 
   constructor(
     private router: Router,

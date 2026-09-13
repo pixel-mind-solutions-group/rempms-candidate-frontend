b0b6715cda29a7d0b49d68676d59c333062257cf
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-09-13 22:57:28 +0530

    brand names changed

--- src/app/containers/default-layout/_nav.ts
+++ src/app/containers/default-layout/_nav.ts
@@ -122,17 +122,4 @@ export const navItems: INavData[] = [
       },
     ],
   },
-  // {
-  //   name: 'Docs',
-  //   url: 'https://coreui.io/angular/docs/templates/installation',
-  //   iconComponent: {name: 'cil-description'},
-  //   attributes: {target: '_blank', class: '-text-dark'},
-  //   class: 'mt-auto'
-  // },
-  // {
-  //   name: 'Try CoreUI PRO',
-  //   url: 'https://coreui.io/product/angular-dashboard-template/',
-  //   iconComponent: {name: 'cil-layers'},
-  //   attributes: {target: '_blank'}
-  // }
 ];
